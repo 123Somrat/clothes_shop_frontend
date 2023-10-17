48f591ce5d3ebaa32aa8538a714243a001a4306a
123Somrat <jafaruddin875@gmail.com>
2023-10-18 00:41:18 +0200

    add banner  vedio autoplay feature

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -13,30 +13,35 @@ import "../../App.css";
 import { Autoplay, Pagination, EffectFade, Navigation } from "swiper/modules";
 
 export default function Banner() {
+const addLinks = ["https://www.youtube.com/embed/_h6aWfAhiis?autoplay=1&mute=1","https://www.youtube.com/embed/b8PUpLsGSN8?autoplay=1&mute=1"]
   return (
     <div className="w-full h-[450px]">
       <Swiper
         spaceBetween={30}
         effect={"fade"}
-        autoplay={{ delay: 3000 }}
+        autoplay={{ delay: 10000 }}
         navigation={true}
         pagination={{
-          clickable: true,
-        }}
+            clickable: true,
+          }}
         modules={[EffectFade, Navigation, Pagination, Autoplay]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <iframe
-            width="1762"
-            height="801"
-            src="https://www.youtube.com/embed/b8PUpLsGSN8?autoplay=1&mute=1"
-            title="30 Second Nike Commercial"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
-        </SwiperSlide>
+        {
+            addLinks.map((vedio,id)=><SwiperSlide key={id}>
+            <iframe
+          
+              width="1762"
+              height="801"
+              src={vedio}
+              title="30 Second Nike Commercial"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </SwiperSlide>)
+        }
+        
       </Swiper>
     </div>
   );
